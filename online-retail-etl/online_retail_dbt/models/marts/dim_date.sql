219d68d8ab647f0887ab06207ccd53f9963d58a3
bolajiemmanuel01 <bolajiemmanuel01@gmail.com>
2025-07-28 10:53:42 +0100

    created Date dimension to slice and dice facts by year, month, day, and weekday.

---
+++ online-retail-etl/online_retail_dbt/models/marts/dim_date.sql
@@ -0,0 +1,16 @@
+-- models/marts/dim_date.sql
+
+{{ config(materialized='table') }}  
+
+
+select
+  invoice_date::date                      as date_key,    -- the surrogate key for the date
+  extract(year  from invoice_date)        as year,        -- reporting year
+  extract(month from invoice_date)        as month,       -- reporting month
+  extract(day   from invoice_date)        as day,         -- day of month
+  to_char(invoice_date, 'Day')            as weekday,     -- name of the weekday
+
+from (
+  select distinct invoice_date from {{ ref('stg_online_retail') }}
+) raw_dates
+order by date_key
